ecc3e48a2528ea82bda2a40ee1994fd04e9dd1ca
Ivan Yakimov <edlinsoft@gmail.com>
2015-09-16 11:26:59 +0300

    Support of endless cards provider is added

---
+++ EdlinSoftware.Cards.Tests/CardsProviderTest.cs
@@ -0,0 +1,18 @@
+﻿using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace EdlinSoftware.Cards.Tests
+{
+    public class EndlessCardsProviderTest
+    {
+        [Theory]
+        [AutoData]
+        public void CardsProvider_ShouldReturnEndlessStreamOfCards([Frozen(As = typeof(IDeckCreator))] FullDeckCreator deckCreator, EndlessCardsProvider cardsProvider)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Assert.NotNull(cardsProvider.Deal());
+            }
+        }
+    }
+}
---
+++ EdlinSoftware.Cards/IEndlessCardsProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EdlinSoftware.Cards
+{
+    /// <summary>
+    /// Provider of endless stream of shuffled cards.
+    /// </summary>
+    public interface IEndlessCardsProvider
+    {
+        /// <summary>
+        /// Returns one card.
+        /// </summary>
+        Card Deal();
+    }
+
+    public class EndlessCardsProvider : IEndlessCardsProvider
+    {
+        private readonly IDeckCreator _deckCreator;
+
+        private Deck _deck;
+
+        [DebuggerStepThrough]
+        public EndlessCardsProvider(IDeckCreator deckCreator)
+        {
+            if (deckCreator == null) throw new ArgumentNullException(nameof(deckCreator));
+            _deckCreator = deckCreator;
+        }
+
+        public Card Deal()
+        {
+            while (_deck == null || _deck.IsEmpty)
+            {
+                _deck = _deckCreator.CreateDeck();
+                _deck.Shuffle();
+            }
+
+            return _deck.Deal();
+        }
+    }
+
+}
